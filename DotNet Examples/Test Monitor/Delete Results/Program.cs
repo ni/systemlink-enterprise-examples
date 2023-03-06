ae8708ed8d3cdf2d5f18dedb5fed263e2b533e14
Madhan Mohan Reddy Peram <madhan.mohan.reddy.peram@ni.com>
2023-03-06 13:31:38 +0530

    Resolved the comments

--- DotNet Examples/Test Monitor/Delete Results/Program.cs
+++ DotNet Examples/Test Monitor/Delete Results/Program.cs
@@ -1,5 +1,4 @@
-﻿using Delete_Results;
-using NationalInstruments.SystemLink.Clients.Examples;
+﻿using NationalInstruments.SystemLink.Clients.Examples.TestMonitor.DeleteResults;
 using NationalInstruments.SystemLink.Clients.TestMonitor;
 using System;
 using System.Collections.Generic;
@@ -48,102 +47,6 @@ namespace NationalInstruments.SystemLink.Clients.Examples.TestMonitor
 
             // Delete the test results
             testDataManager.DeleteResultsAsync(configuration, testResultIds);
-        }
-
-        /// <summary>
-        /// Simulates taking an electrical power measurement.
-        /// This introduces some random current and voltage loss.
-        /// </summary>
-        /// <param name="current">The electrical current value.</param>
-        /// <param name="voltage">The electrical voltage value.</param>
-        /// <returns>The a tuple containing the electrical power measurements and the input and output lists.</returns>
-        private static (double power, List<NamedValue> inputs, List<NamedValue> outputs) MeasurePower(int current, int voltage = 0)
-        {
-            var random = new Random();
-            var currentLoss = 1 - random.NextDouble() * 0.25;
-            var voltageLoss = 1 - random.NextDouble() * 0.25;
-            var power = current * currentLoss * voltage * voltageLoss;
-
-            // Record electrical current and voltage as inputs.
-            var inputs = new List<NamedValue>()
-            {
-                new NamedValue("current", current),
-                new NamedValue("voltage", voltage)
-            };
-
-            // Record electrical power as an output.
-            var outputs = new List<NamedValue>()
-            {
-                new NamedValue("power", power)
-            };
-
-            return (power, inputs, outputs);
-        }
-
-        /// <summary>
-        /// Builds a Test Monitor measurement parameter object for the power test.
-        /// </summary>
-        /// <param name="power">The electrical power measurement.</param>
-        /// <param name="lowLimit">The value of the low limit for the test.</param>
-        /// <param name="highLimit">The value of the high limit for the test.</param>
-        /// <param name="status">The measurement's pass/fail status.</param>
-        /// <returns>A list of test measurement parameters.</returns>
-        private static List<Dictionary<string, string>> BuildPowerMeasurementParams(
-            double power,
-            double lowLimit,
-            double highLimit,
-            Status status)
-        {
-            var parameter = new Dictionary<string, string>()
-            {
-                ["name"] = "Power Test",
-                ["status"] = status.StatusType.ToString(),
-                ["measurement"] = power.ToString(CultureInfo.InvariantCulture),
-                ["units"] = "Watts",
-                ["nominalValue"] = null,
-                ["lowLimit"] = lowLimit.ToString(CultureInfo.InvariantCulture),
-                ["highLimit"] = highLimit.ToString(CultureInfo.InvariantCulture),
-                ["comparisonType"] = "GELE"
-            };
-
-            var parameters = new List<Dictionary<String, String>>() { parameter };
-            return parameters;
-        }
-
-        /// <summary>
-        /// Creates a <see cref="StepData"/> object and
-        /// populates it to match the TestStand data model.
-        /// </summary>
-        /// <param name="name">The test step's name.</param>
-        /// <param name="stepType">The test step's type.</param>
-        /// <param name="inputs">The test step's input values.</param>
-        /// <param name="outputs">The test step's output values.</param>
-        /// <param name="parameters">The measurement parameters.</param>
-        /// <returns>The <see cref="StepData"/> used to create a test step.</returns>
-        private static StepData GenerateStepData(
-            string name,
-            string stepType,
-            List<NamedValue> inputs = null,
-            List<NamedValue> outputs = null,
-            List<Dictionary<String, String>> parameters = null,
-            Status status = null)
-        {
-            var random = new Random();
-            var stepStatus = status ?? new Status(StatusType.Running);
-
-            var stepData = new StepData()
-            {
-                Name = name,
-                Inputs = inputs,
-                Outputs = outputs,
-                StepType = stepType,
-                Status = stepStatus,
-                TotalTimeInSeconds = random.NextDouble() * 10,
-                Parameters = parameters,
-                DataModel = "TestStand",
-            };
-
-            return stepData;
         }
     }
 }
--- DotNet Examples/Test Monitor/Delete Results/TestDataManagerDeleteExtensions.cs
+++ DotNet Examples/Test Monitor/Delete Results/TestDataManagerDeleteExtensions.cs
@@ -5,28 +5,38 @@ using NationalInstruments.SystemLink.Clients.TestMonitor.Swagger.Model;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Delete_Results
+namespace NationalInstruments.SystemLink.Clients.Examples.TestMonitor.DeleteResults
 {
-    static class Extensions
+    static class TestDataManagerDeleteExtensions
     {
+        private const string DeleteResultsPath = "nitestmonitor/v2/delete-results";
+        private const string DeleteResultPath = "nitestmonitor/v2/results";
         public static HttpResponseMessage DeleteResultAsync(this TestDataManager testDataManager, IHttpConfiguration configuration, string resultId, bool? deleteSteps = true)
         {
             if (resultId == null)
             {
                 throw new ApiException("Missing required parameter 'resultId' when calling ResultsApi->DeleteResultV2");
             }
-            string path = $"nitestmonitor/v2/results/{resultId}?deleteSteps={deleteSteps}";
+            string path = $"{DeleteResultPath}/{resultId}?deleteSteps={deleteSteps}";
 
+            return CallDeleteResultApi(configuration, path);
+        }
+
+        private static HttpResponseMessage CallDeleteResultApi(IHttpConfiguration configuration, string requestUri)
+        {
             HttpClient client = GetHttpClient(configuration);
 
-            var response = client.DeleteAsync(path).Result;
+            var response = client.DeleteAsync(requestUri).Result;
             response.EnsureSuccessStatusCode();
+
             return response;
         }
 
@@ -37,20 +47,30 @@ namespace Delete_Results
             {
                 throw new ApiException("ids is a required property for DeleteResultsRequest and cannot be null");
             }
-            string path = $"nitestmonitor/v2/delete-results";
 
-            var content = new
+            var bodyContent = new
             {
                 Ids = resultIds.ToArray(),
                 DeleteSteps = deleteSteps
             };
 
-            var body = new StringContent(JsonConvert.SerializeObject(content), Encoding.UTF8, "application/json");
+            var body = GeneratePostRequestBody(bodyContent);
 
+            return CallDeleteResultsApi(configuration, DeleteResultsPath, body);
+        }
+
+        private static StringContent GeneratePostRequestBody(object content)
+        {
+            return new StringContent(JsonConvert.SerializeObject(content), Encoding.UTF8, "application/json");
+        }
+
+        private static HttpResponseMessage CallDeleteResultsApi(IHttpConfiguration configuration, string requestUri, HttpContent body)
+        {
             HttpClient client = GetHttpClient(configuration);
 
-            var response = client.PostAsync(path, body).Result;
+            var response = client.PostAsync(DeleteResultsPath, body).Result;
             response.EnsureSuccessStatusCode();
+
             return response;
         }
 
@@ -59,6 +79,7 @@ namespace Delete_Results
             var client = new HttpClient();
             client.BaseAddress = new Uri(configuration.ServerUri.AbsoluteUri);
             client.DefaultRequestHeaders.Add("x-ni-api-key", configuration.ApiKeys["x-ni-api-key"]);
+
             return client;
         }
     }
