789d6a5f4d24a27b71074e31ee07fbef1e5793bd
Madhan Mohan Reddy Peram <madhan.mohan.reddy.peram@ni.com>
2023-03-08 15:46:03 +0530

    Resolved the comments

--- DotNet Examples/Test Monitor/DeleteResults/DeleteResults.cs
+++ DotNet Examples/Test Monitor/DeleteResults/DeleteResults.cs
@@ -1,12 +1,10 @@
-﻿using NationalInstruments.SystemLink.Clients.Examples.TestMonitor.DeleteResults;
-using NationalInstruments.SystemLink.Clients.TestMonitor;
+﻿using NationalInstruments.SystemLink.Clients.TestMonitor;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 
-namespace NationalInstruments.SystemLink.Clients.Examples.TestMonitor
+namespace NationalInstruments.SystemLink.Clients.Examples.TestMonitor.DeleteResults
 {
-    class Program
+    class DeleteResults
     {
         static void Main(string[] args)
         {
@@ -32,21 +30,31 @@ namespace NationalInstruments.SystemLink.Clients.Examples.TestMonitor
             };
             // Create the test result.
             var testResult = testDataManager.CreateResult(resultData);
+            Console.WriteLine($"The test result has been under part number={testResult.Data.PartNumber} with Id = {testResult.Id}");
+            Console.WriteLine("Press any key to delete the result");
+            Console.ReadKey();
 
             // Delete the test result
             testDataManager.DeleteResultAsync(configuration, testResult.Id);
+            Console.WriteLine($"\nThe test result with Id = {testResult.Id} has been deleted");
 
             // Create multiple test results
+            Console.WriteLine("\nCreating multiple test results.\nResult Ids has been listed down below");
             var testResultIds = new List<string>();
             for(int i = 0;i<5;i++)
             {
                 resultData.ProgramName = resultData.ProgramName + i;
                 var result = testDataManager.CreateResult(resultData);
                 testResultIds.Add(result.Id);
+                Console.WriteLine($"{result.Id}");
             }
 
             // Delete the test results
+            Console.WriteLine("\nThe multiple results has been created successfully");
+            Console.WriteLine("Please any key to delete these results");
+            Console.ReadKey();
             testDataManager.DeleteResultsAsync(configuration, testResultIds);
+            Console.WriteLine("\nResults has been deleted successfully");
         }
     }
 }
