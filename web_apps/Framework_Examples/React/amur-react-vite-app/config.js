b1de94aa2753c034f838f2f7d311cea22bd685c0
Amur Bashirov <bashirov@byu.edu>
2026-01-23 11:52:14 -0700

    changed it to the typescript

--- web_apps/Framework_Examples/React/amur-react-vite-app/config.js
+++
@@ -1,4 +0,0 @@
-module.exports =  {
-    api_key: "place your api key here"
-    // don't forget to put this file into gitignore before puting your api key here
-}
---
+++ web_apps/Framework_Examples/React/amur-react-vite-app/config.ts
@@ -0,0 +1,12 @@
+// config.ts
+const config = {
+  api_key: "place your api key here",
+};
+
+export default config;
+
+
+// the way to use it
+// import config from "./config";
+
+// console.log(config.apiKey);
