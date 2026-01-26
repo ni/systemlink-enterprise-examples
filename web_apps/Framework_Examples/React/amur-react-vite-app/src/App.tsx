4edeaad17695b874b43dab8ffee26ce1fab2ede2
Bowen Liu <reouct@gmail.com>
2026-01-26 16:34:52 -0700

    CSS cleanup.

--- web_apps/Framework_Examples/React/amur-react-vite-app/src/App.tsx
+++ web_apps/Framework_Examples/React/amur-react-vite-app/src/App.tsx
@@ -19,21 +19,21 @@ function App() {
       API call example
     </h2>
       <div className='main_description'>
-
-
         <div className='API'>
-
-        <div className='first_line'>
+        <div className='API_method_and_path'>
           <span className="method">GET</span>
             <h3 className="path">/auth</h3>
-          <span className='mini_description'>Authenticates API Keys</span>
         </div>
-        
-        <div className='second_line'>
-          <span className='text'>
-            The example makes an HTTP GET request to a SystemLink API and displays the result on the page.
-             This specific API Authenticates the given x-ni-api-key and returns information about the call.
+        <div>
+          <span className='API_description_title'>Authenticates API Keys</span>
+        </div>
+        <div className='API_description'>
+          <span className='API_text'>
+            The example makes an HTTP GET request to a SystemLink API and displays the result on the page.This specific API Authenticates the given x-ni-api-key and returns information about the call.
           </span>
+        </div>
+        <div className='API_response'>
+          
         </div>
 
         </div>
