610c69026e6e756f1d2731d701a80f1ef36bfb27
samuel <samuelsotointernational@gmail.com>
2026-02-08 02:32:39 -0700

    Installed NimbleBlazor dependencies, created API component page with a Nimble button, fixed some typos, and made Nimble css and Nimble script available to all components in App.razor

--- web_apps/Framework_Examples/Blazor/BlazorApp1/obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
+++ web_apps/Framework_Examples/Blazor/BlazorApp1/obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af084c00be18e269610a735603631e464d980d0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
