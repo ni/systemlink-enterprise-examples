fedfca47d319eddc6c3382afc94a514ee4499666
Kurt Rowley <bensonro@byu.edu>
2026-02-20 12:22:52 -0700

    fixed eslint config (app ts/tsx, js, and vite errors)

--- examples/Web Applications/Framework Examples/React/ApiKeyAuthApp/eslint.config.js
+++ examples/Web Applications/Framework Examples/React/ApiKeyAuthApp/eslint.config.js
@@ -1,15 +1,79 @@
 import { defineConfig } from "eslint/config";
+import react from "eslint-plugin-react";
+
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+
+import { javascriptConfig } from "@ni/eslint-config-javascript";
 import { typescriptConfig } from "@ni/eslint-config-typescript";
 
 export default defineConfig([
   {
-    files: ["**/*.ts"],
-    extends: typescriptConfig,
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: {
+      react,
+      "react-refresh": reactRefresh,
+      "react-hooks": reactHooks,
+    },
+    extends: [
+      typescriptConfig,
+      react.configs.flat.recommended,
+      react.configs.flat["jsx-runtime"],
+      reactHooks.configs.flat.recommended,
+    ],
     languageOptions: {
       parserOptions: {
-        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        project: "./tsconfig.app.json",
         tsconfigRootDir: import.meta.dirname,
+        ecmaFeatures: {
+          jsx: true,
+        },
       },
     },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      // The React components should use PascalCase
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector: "variable",
+          format: ["camelCase", "PascalCase"],
+        },
+      ],
+      "react-refresh/only-export-components": [
+        "error",
+        { allowConstantExport: true },
+      ],
+      // "@typescript-eslint/strict-boolean-expressions": "off",
+      // "no-alert": "off",
+    },
+  },
+
+  {
+    files: ["vite.config.ts"],
+    extends: [typescriptConfig],
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.node.json",
+        tsconfigRootDir: import.meta.dirname.pathname,
+      },
+    },
+    rules: {
+      // Configuration scripts will not be in published package and are allowed to use devDependencies
+      "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+      // Build scripts should give verbose logging
+      "no-console": "off",
+      // Rollup config files use default exports
+      "import/no-default-export": "off",
+    },
+  },
+
+  {
+    files: ["**/*.js"],
+    extends: [javascriptConfig],
   },
 ]);
