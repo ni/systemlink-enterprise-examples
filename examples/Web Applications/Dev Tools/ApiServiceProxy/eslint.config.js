b7d4b51e208be27a9be3b2e3d37e0e6fe28259b5
Kurt Rowley <bensonro@byu.edu>
2026-03-16 09:43:59 -0600

    add eslint to apiProxy

---
+++ examples/Web Applications/Dev Tools/ApiServiceProxy/eslint.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'eslint/config';
+
+import { javascriptConfig } from '@ni/eslint-config-javascript';
+
+export default defineConfig([
+    {
+        files: ['**/*.js'],
+        extends: [javascriptConfig],
+    },
+]);
