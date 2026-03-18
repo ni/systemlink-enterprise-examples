95b600f5a3aaccb711533de120a6f826a06a7374
Amur Bashirov <bashirov@byu.edu>
2026-03-18 11:20:48 -0600

    installed dependency for sans pro

--- examples/Web Applications/Framework Examples/React/ApiKeyAuthApp/src/main.tsx
+++ examples/Web Applications/Framework Examples/React/ApiKeyAuthApp/src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
+import '@fontsource/source-sans-pro/400.css';
+import '@fontsource/source-sans-pro/600.css';
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <App />
