6845a8d5cea060a7607af0ca2480ebb7b6e218ed
Kurt Rowley <bensonro@byu.edu>
2026-03-18 10:28:58 -0600

    fix merge conflicts

--- examples/Web Applications/Framework Examples/React/ApiKeyAuthApp/src/main.tsx
+++ examples/Web Applications/Framework Examples/React/ApiKeyAuthApp/src/main.tsx
@@ -1,21 +1,9 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { App } from './App';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
 
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
-);
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
->>>>>>> Blazor
+);
