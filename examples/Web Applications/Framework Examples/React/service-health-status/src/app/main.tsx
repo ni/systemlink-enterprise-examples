a71ebdf0614552e294aeb3b426d7432622cae2bf
Bowen Liu <reouct@gmail.com>
2026-03-24 10:32:39 -0600

    update main background color with nimble

--- examples/Web Applications/Framework Examples/React/service-health-status/src/app/main.tsx
+++ examples/Web Applications/Framework Examples/React/service-health-status/src/app/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import ServiceHealthDashboard from './ServiceHealthDashboard';
-import '../styles/main.css';
+import '../styles/main.scss';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
