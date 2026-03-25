14a7c6d4361047d2373c67bb86d7ed4bb43d2142
Bowen Liu <reouct@gmail.com>
2026-03-25 11:44:03 -0600

    Fix fonts

--- examples/Web Applications/Framework Examples/React/service-health-status/src/app/main.tsx
+++ examples/Web Applications/Framework Examples/React/service-health-status/src/app/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import { ServiceHealthDashboard } from './ServiceHealthDashboard';
 import '../styles/main.scss';
 
+import '@fontsource/source-sans-pro/400.css';
+import '@fontsource/source-sans-pro/600.css';
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <ServiceHealthDashboard />
