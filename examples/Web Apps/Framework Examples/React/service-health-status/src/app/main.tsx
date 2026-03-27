5d247b907ea84b15d7882ff60441c2d8696d755c
Bowen Liu <reouct@gmail.com>
2026-03-27 10:44:18 -0600

    Remove the outside package for the fonts

--- examples/Web Apps/Framework Examples/React/service-health-status/src/app/main.tsx
+++ examples/Web Apps/Framework Examples/React/service-health-status/src/app/main.tsx
@@ -1,16 +1,13 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { NimbleThemeProvider } from '@ni/nimble-react/theme-provider';
-import { ServiceHealthDashboard } from './ServiceHealthDashboard';
-import '../styles/main.scss';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { NimbleThemeProvider } from "@ni/nimble-react/theme-provider";
+import { ServiceHealthDashboard } from "./ServiceHealthDashboard";
+import "../styles/main.scss";
 
-import '@fontsource/source-sans-pro/400.css';
-import '@fontsource/source-sans-pro/600.css';
-
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <NimbleThemeProvider theme="light">
-            <ServiceHealthDashboard />
-        </NimbleThemeProvider>
-    </StrictMode>,
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <NimbleThemeProvider theme="light">
+      <ServiceHealthDashboard />
+    </NimbleThemeProvider>
+  </StrictMode>,
 );
