de1978cb4e5668a07aded8162bc316d31fa88a81
Bowen Liu <reouct@gmail.com>
2026-03-27 10:46:08 -0600

    fix lint

--- examples/Web Apps/Framework Examples/React/service-health-status/src/app/main.tsx
+++ examples/Web Apps/Framework Examples/React/service-health-status/src/app/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { NimbleThemeProvider } from "@ni/nimble-react/theme-provider";
-import { ServiceHealthDashboard } from "./ServiceHealthDashboard";
-import "../styles/main.scss";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { NimbleThemeProvider } from '@ni/nimble-react/theme-provider';
+import { ServiceHealthDashboard } from './ServiceHealthDashboard';
+import '../styles/main.scss';
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <NimbleThemeProvider theme="light">
-      <ServiceHealthDashboard />
-    </NimbleThemeProvider>
-  </StrictMode>,
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <NimbleThemeProvider theme="light">
+            <ServiceHealthDashboard />
+        </NimbleThemeProvider>
+    </StrictMode>,
 );
