e309a062e1179f7ec20e3eb3ddc0613b8ce0edd6
Amur Bashirov <bashirov@byu.edu>
2026-04-09 09:20:46 -0600

    got rid of font imports and cleaned up module imports

--- examples/Web Apps/Framework Examples/Angular/SystemLinkAuthExample/src/app/app.module.ts
+++ examples/Web Apps/Framework Examples/Angular/SystemLinkAuthExample/src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NimbleButtonModule, NimbleDrawerModule, NimbleThemeProviderModule,} from '@ni/nimble-angular';
+import { NimbleButtonModule, NimbleThemeProviderModule,} from '@ni/nimble-angular';
 
 @NgModule({
   declarations: [
@@ -11,8 +10,6 @@ import { NimbleButtonModule, NimbleDrawerModule, NimbleThemeProviderModule,} fro
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NimbleDrawerModule,
     NimbleThemeProviderModule,
     NimbleButtonModule
   ],
